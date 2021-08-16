eb49090a313e94b53d16fb493cfe80a6dbb871dc
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-08-15 22:46:01 -0300

    base project

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import {useState} from "react";
+
+export function Button() {
+    let [counter, setCounter] = useState(0)
+
+    function increment() {
+        setCounter(++counter)
+    }
+
+    return (
+        <button onClick={increment}>{counter}</button>
+    )
+}
